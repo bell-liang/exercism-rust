a67e500eda369215a4e2f9c96b5f5a27f993b7dd
bell_liang <liang.foreverbell@qq.com>
2019-12-13 21:45:31 +0800

    练习题 2、 3、 4

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,3 +1,69 @@
+// 1 输出 Hello world!
 pub fn hello() -> &'static str {
     "Hello, World!"
  }
+
+// 2 返回指定时间1亿秒后的时间
+use chrono::{DateTime, Duration, Utc};
+
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    start + Duration::seconds(1_000_000_000)
+}
+
+// 3 判定指定年是否闰年
+pub fn is_leap_year(year: i32) -> bool {
+    if year % 100 != 0 && year % 4 == 0 {
+        true
+    } else if year % 100 == 0 && year % 400 == 0 {
+        true
+    } else {
+        false
+    }
+}
+/*
+pub fn is_leap_year(year: i32) -> bool {
+   let has_factor = |n| year % n == 0;
+   has_factor(4) && (!has_factor(100) || has_factor(400))
+}
+*/
+
+/* 4 雨滴-Raindrops
+    把一个数字转换成一个字符串,它的内容取决于，数字的因素.
+    如果数字有 3 作为一个因素,输出”Pling”.
+    如果数字有 5 作为一个因素,输出”Plang’”.
+    如果数字有 7 作为一个因素,输出”Plong”.
+    如果数字没有 3, 5,或 7 作为一个因素, 直接给数字
+*/
+pub fn raindrops(n: u32) -> String {
+    if n % 3 == 0 {
+        String::from("Pling")
+    } else if n % 5 == 0 {
+        String::from("Plang")
+    } else if n % 7 == 0 {
+        String::from("Plong")
+    } else {
+        String::from(n.to_string())
+    }
+}
+/*
+pub fn raindrops(n: u32) -> String {
+   let is_pling = |n| n % 3 == 0;
+   let is_plang = |n| n % 5 == 0;
+   let is_plong = |n| n % 7 == 0;
+   let mut drops = String::new();
+   if is_pling(n) {
+       drops.push_str("Pling");
+   }
+   if is_plang(n) {
+       drops.push_str("Plang");
+   }
+   if is_plong(n) {
+       drops.push_str("Plong");
+   }
+   if drops.is_empty() {
+       let s = format!("{}", n);
+       drops.push_str(&s);
+   }
+   drops
+}
+*/
--- tests/simple_test.rs
+++ tests/simple_test.rs
@@ -1,7 +1,204 @@
 mod simple;
 use simple::*;
 
+// 1
 #[test]
 fn test_hello_world() {
 	assert_eq!("Hello, World!", hello());
+}
+
+// 2
+use chrono::{Utc, TimeZone};
+
+#[test]
+fn test_date() {
+   let start_date = Utc.ymd(2011, 4, 25).and_hms(0, 0, 0);
+
+   assert_eq!(after(start_date), Utc.ymd(2043, 1, 1).and_hms(1, 46, 40));
+}
+
+#[test]
+//#[ignore]
+fn test_another_date() {
+   let start_date = Utc.ymd(1977, 6, 13).and_hms(0, 0, 0);
+
+   assert_eq!(after(start_date), Utc.ymd(2009, 2, 19).and_hms(1, 46, 40));
+}
+
+#[test]
+//#[ignore]
+fn test_third_date() {
+   let start_date = Utc.ymd(1959, 7, 19).and_hms(0, 0, 0);
+
+   assert_eq!(after(start_date), Utc.ymd(1991, 3, 27).and_hms(1, 46, 40));
+}
+
+#[test]
+//#[ignore]
+fn test_datetime() {
+   let start_date = Utc.ymd(2015, 1, 24).and_hms(22, 0, 0);
+
+   assert_eq!(after(start_date), Utc.ymd(2046, 10, 2).and_hms(23, 46, 40));
+}
+
+#[test]
+//#[ignore]
+fn test_another_datetime() {
+   let start_date = Utc.ymd(2015, 1, 24).and_hms(23, 59, 59);
+
+   assert_eq!(after(start_date), Utc.ymd(2046, 10, 3).and_hms(1, 46, 39));
+}
+
+// 3
+#[test]
+fn test_vanilla_leap_year() {
+   assert_eq!(is_leap_year(1996), true);
+}
+
+#[test]
+//#[ignore]
+fn test_any_old_year() {
+   assert_eq!(is_leap_year(1997), false);
+}
+
+#[test]
+//#[ignore]
+fn test_century() {
+   assert_eq!(is_leap_year(1700), false);
+   assert_eq!(is_leap_year(1800), false);
+   assert_eq!(is_leap_year(1900), false);
+}
+
+#[test]
+//#[ignore]
+fn test_exceptional_centuries() {
+   assert_eq!(is_leap_year(1600), true);
+   assert_eq!(is_leap_year(2000), true);
+   assert_eq!(is_leap_year(2400), true);
+}
+
+#[test]
+//#[ignore]
+fn test_years_1600_to_1699() {
+   let incorrect_years = (1600..1700)
+       .filter(|&year| is_leap_year(year) != (year % 4 == 0))
+       .collect::<Vec<_>>();
+
+   if !incorrect_years.is_empty() {
+       panic!("incorrect result for years: {:?}", incorrect_years);
+   }
+}
+
+// 4
+#[test]
+fn test_1() {
+   assert_eq!("1", raindrops(1));
+}
+
+#[test]
+//#[ignore]
+fn test_3() {
+   assert_eq!("Pling", raindrops(3));
+}
+
+#[test]
+//#[ignore]
+fn test_5() {
+   assert_eq!("Plang", raindrops(5));
+}
+
+#[test]
+//#[ignore]
+fn test_7() {
+   assert_eq!("Plong", raindrops(7));
+}
+
+#[test]
+//#[ignore]
+fn test_6() {
+   assert_eq!("Pling", raindrops(6));
+}
+
+#[test]
+//#[ignore]
+fn test_8() {
+   assert_eq!("8", raindrops(8));
+}
+
+#[test]
+//#[ignore]
+fn test_9() {
+   assert_eq!("Pling", raindrops(9));
+}
+
+#[test]
+//#[ignore]
+fn test_10() {
+   assert_eq!("Plang", raindrops(10));
+}
+
+#[test]
+//#[ignore]
+fn test_14() {
+   assert_eq!("Plong", raindrops(14));
+}
+
+#[test]
+//#[ignore]
+fn test_15() {
+   assert_eq!("PlingPlang", raindrops(15));
+}
+
+#[test]
+//#[ignore]
+fn test_21() {
+   assert_eq!("PlingPlong", raindrops(21));
+}
+
+#[test]
+//#[ignore]
+fn test_25() {
+   assert_eq!("Plang", raindrops(25));
+}
+
+#[test]
+//#[ignore]
+fn test_27() {
+   assert_eq!("Pling", raindrops(27));
+}
+
+#[test]
+//#[ignore]
+fn test_35() {
+   assert_eq!("PlangPlong", raindrops(35));
+}
+
+#[test]
+//#[ignore]
+fn test_49() {
+   assert_eq!("Plong", raindrops(49));
+}
+
+#[test]
+//#[ignore]
+fn test_52() {
+   assert_eq!("52", raindrops(52));
+}
+
+#[test]
+//#[ignore]
+fn test_105() {
+   assert_eq!("PlingPlangPlong", raindrops(105));
+}
+
+#[test]
+//#[ignore]
+fn test_3125() {
+   assert_eq!("Plang", raindrops(3125));
+}
+
+#[test]
+//#[ignore]
+fn test_12121() {
+   assert_eq!("12121", raindrops(12121));
 }
