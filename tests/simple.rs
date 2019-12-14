4f37b0029256bfb4bfb451f9eb74cba95066afbe
bell_liang <liang.foreverbell@qq.com>
2019-12-14 21:07:28 +0800

    习题 simple 4-12

--- tests/simple.rs
+++ tests/simple.rs
@@ -67,3 +67,262 @@ pub fn raindrops(n: u32) -> String {
    drops
 }
 */
+
+// 5 翻转字符串
+pub fn reverse(input: &str) -> String {
+    let mut string = String::new();
+    for i in input.chars().rev() {
+        string.push(i);
+    }
+    string
+}
+/*
+pub fn reverse(input: &str) -> String {
+   let mut output = String::with_capacity(input.len());
+   output.extend(input.chars().rev());
+   output
+}
+*/
+
+// 6 第 n 个素数
+pub fn nth(n: u32) -> u32 {
+    if n == 0 {
+        return 2
+    }
+    let mut count = 0;
+    let is_sushu = |x| {
+        let mut result = true;
+        for i in 2..x {
+            if x % i == 0 {
+                result = false;
+                break
+            }
+        }
+        result
+    };
+    let mut goal = 0;
+    for i in 3.. {
+        if is_sushu(i) {
+            count += 1;
+            if n == count {
+                goal = i;
+                break
+            }
+        }
+    }
+    goal
+}
+/*
+fn is_prime(n: u32) -> bool {
+   let mut i = 3;
+   while (i * i) < (n + 1) {
+       if n % i == 0 {
+           return false;
+       }
+       i += 1;
+   }
+   return true;
+}
+
+pub fn nth(n: u32) -> u32 {
+   if n == 0 {
+       2
+   } else {
+       let mut count = 0;
+       let mut candidate = 1;
+       while count < n {
+           candidate += 2;
+           if is_prime(candidate) {
+               count += 1;
+           }
+       }
+       candidate
+   }
+}
+*/
+
+/* 7 鲍勃-bob
+鲍伯是一个懒散的青少年.在谈话中,他的反应非常有限.
+
+鲍伯回答:”Sure.”，如果你问他一个问题.
+
+他回答:”Whoa, chill out!”，如果你对他大喊大叫.
+
+他回答”Calm down, I know what I’m doing!”，如果你大声问他问题.
+
+他说”Fine. Be that way!”，如果你喊他,而不说任何话.
+
+他回答”Whatever”，给剩下的对话
+
+鲍勃的对话伙伴，在书面交流方面是一个纯粹主义者,并且总是遵循关于 英语句子标点 的通用规则.
+*/
+pub fn reply(message: &str) -> &str {
+    let is_question = |s: &str| s.trim_end().ends_with('?');
+    let is_yelling = |s: &str| {
+        let s = s.trim_matches(|c: char| !c.is_alphabetic());
+        !s.is_empty() && s.to_uppercase() == s
+    };
+    let is_silence = |s: &str| {
+        s.trim().is_empty()
+    };
+
+    if is_silence(message) {
+        "Fine. Be that way!"
+    } else if is_yelling(message) && is_question(message) {
+        "Calm down, I know what I'm doing!"
+    } else if is_yelling(message) {
+        "Whoa, chill out!"
+    } else if is_question(message) {
+        "Sure."
+    } else {
+        "Whatever."
+    }
+}
+
+// 8 啤酒之歌
+pub fn verse(n: i32) -> String {
+    match n {
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\n\
+              Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+            .to_string(),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\n\
+              Take it down and pass it around, no more bottles of beer on the wall.\n"
+            .to_string(),
+        2 => "2 bottles of beer on the wall, 2 bottles of beer.\n\
+              Take one down and pass it around, 1 bottle of beer on the wall.\n"
+            .to_string(),
+        n if n > 2 && n <= 99 => format!(
+            "{n} bottles of beer on the wall, {n} bottles of beer.\n\
+             Take one down and pass it around, {n_minus_1} bottles of beer on the wall.\n",
+            n = n,
+            n_minus_1 = n - 1
+        ),
+        _ => panic!(),
+    }
+}
+ 
+pub fn sing(start: i32, end: i32) -> String {
+    (end..start + 1)
+        .rev()
+        .map(|n| verse(n))
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+// 9 谚语
+pub fn build_proverb(list: Vec<&str>) -> String {
+    if list.len() == 0 {
+        return String::new()
+    }
+    if list.len() == 1 {
+        return String::from(format!("And all for the want of a {}.", list[0]))
+    }
+    let format = |(first, second)| {
+        format!("For want of a {} the {} was lost.", first, second)
+    };
+    let mut expected: Vec<String> = Vec::with_capacity(list.len());
+    for i in 0..list.len()-1 {
+        expected.push(format((list[i], list[i+1])));
+    }
+    expected.push(format!("And all for the want of a {}.", list[0]));
+    expected.join("\n")
+}
+/*
+pub fn build_proverb(items: Vec<&str>) -> String {
+   let mut stanzas = Vec::with_capacity(items.len());
+   for index in 0..items.len() {
+       if index == items.len() - 1 {
+           stanzas.push(format!("And all for the want of a {}.", items[0]));
+       } else {
+           stanzas.push(format!(
+               "For want of a {} the {} was lost.",
+               items[index],
+               items[index + 1]
+           ));
+       }
+   }
+   stanzas.join("\n")
+}
+*/
+
+// 10 求，前n个自然数的和平方，与，平方和，之间的差值
+pub fn square_of_sum(n: u32) -> u32 {
+    let result: u32 = (1..n+1).sum();
+    result * result
+ }
+ 
+ pub fn sum_of_squares(n: u32) -> u32 {
+    (1..n+1).map(|i| i * i).collect::<Vec<u32>>().iter().sum()
+ }
+ 
+ pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+ }
+ /*
+ pub fn square_of_sum(n: u32) -> u32 {
+   let sum = n * (n + 1) / 2;
+   sum * sum
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+   (0..n + 1).map(|x| x * x).fold(0, |accum, x| accum + x)
+}
+
+pub fn difference(n: u32) -> u32 {
+   square_of_sum(n) - sum_of_squares(n)
+}
+ */
+
+/* 11 倍数之和
+给定一个数字,找出另外的特定数字的所有唯一倍数的总和,但不包括第一个数字.
+如果我们列出20以下，3或5的倍数的所有自然数,我们得到3,5,6,9,10,12,15和18.
+这些倍数的总和是78
+*/
+use std::collections::BTreeSet;
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let mut multiples: BTreeSet<u32> = BTreeSet::new();
+
+    for i in factors {
+        let mut multiple = 2;
+        let mut x = *i;
+        while x < limit {
+            multiples.insert(x);
+            x = i * multiple;
+            multiple += 1;
+        }
+    }
+
+    multiples.iter().sum()
+}
+
+/* 12 谷物
+    计算棋盘上的小麦粒数,假设每个方格的数量增加一倍.
+    曾经有一位睿智的仆人拯救了王子的生命。国王承诺支付仆人梦寐以求的一切。知道国王喜欢国际象棋,仆人告诉国王他想吃小麦粒，在棋盘的第一个正方形上放一粒小麦。而接下来的方格是两粒.四粒小麦放在第三格,依此类推.
+    棋盘上有 64 个方格.
+    编写代码，用来显示:
+        每个方格上有多少谷物,和
+        谷物总数
+*/
+pub fn square(s: u32) -> u64 {
+    let mut result: u64 = 1;
+    for _ in 0..s-1 {
+        result *= 2
+    }
+    result
+}
+pub fn total() -> u64 {
+    (1..=64).map(|s| square(s)).fold(0, |accum, s| accum + s)
+}
+/*
+pub fn square(s: u32) -> u64 {
+   if s == 0 || s > 64 {
+       panic!("Square must be between 1 and 64");
+   }
+
+   2u64.pow(s - 1)
+}
+
+pub fn total() -> u64 {
+   (1..65).fold(0, |acc, s| acc + square(s))
+}
+*/
--- tests/simple_test.rs
+++ tests/simple_test.rs
@@ -202,3 +202,548 @@ fn test_3125() {
 fn test_12121() {
    assert_eq!("12121", raindrops(12121));
 }
+
+// 4
+fn process_reverse_case(input: &str, expected: &str) {
+    assert_eq!(&reverse(input), expected)
+ }
+ 
+ #[test]
+ /// empty string
+ fn test_empty_string() {
+    process_reverse_case("", "");
+ }
+ 
+ #[test]
+ //#[ignore]
+ /// a word
+ fn test_a_word() {
+    process_reverse_case("robot", "tobor");
+ }
+ 
+ #[test]
+ //#[ignore]
+ /// a capitalized word
+ fn test_a_capitalized_word() {
+    process_reverse_case("Ramen", "nemaR");
+ }
+ 
+ #[test]
+ //#[ignore]
+ /// a sentence with punctuation
+ fn test_a_sentence_with_punctuation() {
+    process_reverse_case("I'm hungry!", "!yrgnuh m'I");
+ }
+ 
+ #[test]
+ //#[ignore]
+ /// a palindrome
+ fn test_a_palindrome() {
+    process_reverse_case("racecar", "racecar");
+ }
+ 
+ #[test]
+ //#[ignore]
+ /// wide characters
+ fn test_wide_characters() {
+    process_reverse_case("子猫", "猫子");
+ }
+ 
+ #[test]
+ //#[ignore]
+ #[cfg(feature = "grapheme")]
+ /// grapheme clusters
+ fn test_grapheme_clusters() {
+    process_reverse_case("uüu", "uüu");
+ }
+
+ // 6
+ #[test]
+fn test_first_prime() {
+   assert_eq!(nth(0), 2);
+}
+
+#[test]
+//#[ignore]
+fn test_second_prime() {
+   assert_eq!(nth(1), 3);
+}
+
+#[test]
+//#[ignore]
+fn test_sixth_prime() {
+   assert_eq!(nth(5), 13);
+}
+
+#[test]
+//#[ignore]
+fn test_big_prime() {
+   assert_eq!(nth(100), 104743);
+}
+
+// 7
+#[test]
+fn test_stating_something() {
+   assert_eq!("Whatever.", reply("Tom-ay-to, tom-aaaah-to."));
+}
+
+#[test]
+//#[ignore]
+fn test_shouting() {
+   assert_eq!("Whoa, chill out!", reply("WATCH OUT!"));
+}
+
+#[test]
+//#[ignore]
+fn test_shouting_gibberish() {
+   assert_eq!("Whoa, chill out!", reply("FCECDFCAAB"));
+}
+
+#[test]
+//#[ignore]
+fn test_asking() {
+   assert_eq!(
+       "Sure.",
+       reply("Does this cryogenic chamber make me look fat?")
+   );
+}
+
+#[test]
+//#[ignore]
+fn test_ask_numeric_question() {
+   assert_eq!("Sure.", reply("You are, what, like 15?"));
+}
+
+#[test]
+//#[ignore]
+fn test_asking_gibberish() {
+   assert_eq!("Sure.", reply("fffbbcbeab?"));
+}
+
+#[test]
+//#[ignore]
+fn test_exclaiming() {
+   assert_eq!("Whatever.", reply("Let's go make out behind the gym!"));
+}
+
+#[test]
+//#[ignore]
+fn test_using_acronyms_in_regular_speech() {
+   assert_eq!(
+       "Whatever.",
+       reply("It's OK if you don't want to go to the DMV.")
+   );
+}
+
+#[test]
+//#[ignore]
+fn test_forceful_question() {
+   assert_eq!(
+       "Calm down, I know what I'm doing!",
+       reply("WHAT THE HELL WERE YOU THINKING?")
+   );
+}
+
+#[test]
+//#[ignore]
+fn test_shouting_numbers() {
+   assert_eq!("Whoa, chill out!", reply("1, 2, 3 GO!"));
+}
+
+#[test]
+//#[ignore]
+fn test_only_numbers() {
+   assert_eq!("Whatever.", reply("1, 2, 3"));
+}
+
+#[test]
+//#[ignore]
+fn test_question_with_only_numbers() {
+   assert_eq!("Sure.", reply("4?"));
+}
+
+#[test]
+//#[ignore]
+fn test_shouting_with_special_characters() {
+   assert_eq!(
+       "Whoa, chill out!",
+       reply("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!")
+   );
+}
+
+#[test]
+//#[ignore]
+fn test_shouting_with_no_exclamation_mark() {
+   assert_eq!("Whoa, chill out!", reply("I HATE YOU"));
+}
+
+#[test]
+//#[ignore]
+fn test_statement_containing_question_mark() {
+   assert_eq!("Whatever.", reply("Ending with ? means a question."));
+}
+
+#[test]
+//#[ignore]
+fn test_non_letters_with_question() {
+   assert_eq!("Sure.", reply(":) ?"));
+}
+
+#[test]
+//#[ignore]
+fn test_prattling_on() {
+   assert_eq!("Sure.", reply("Wait! Hang on. Are you going to be OK?"));
+}
+
+#[test]
+//#[ignore]
+fn test_silence() {
+   assert_eq!("Fine. Be that way!", reply(""));
+}
+
+#[test]
+//#[ignore]
+fn test_prolonged_silence() {
+   assert_eq!("Fine. Be that way!", reply("          "));
+}
+
+#[test]
+//#[ignore]
+fn test_alternate_silence() {
+   assert_eq!("Fine. Be that way!", reply("\t\t\t\t\t\t\t\t\t\t"));
+}
+
+#[test]
+//#[ignore]
+fn test_multiple_line_question() {
+   assert_eq!(
+       "Whatever.",
+       reply("\nDoes this cryogenic chamber make me look fat?\nno")
+   );
+}
+
+#[test]
+//#[ignore]
+fn test_starting_with_whitespace() {
+   assert_eq!("Whatever.", reply("         hmmmmmmm..."));
+}
+
+#[test]
+//#[ignore]
+fn test_ending_with_whitespace() {
+   assert_eq!("Sure.", reply("Okay if like my  spacebar  quite a bit?   "));
+}
+
+#[test]
+//#[ignore]
+fn test_other_whitespace() {
+   assert_eq!("Fine. Be that way!", reply("\n\r \t"));
+}
+
+#[test]
+//#[ignore]
+fn test_non_question_ending_with_whitespace() {
+   assert_eq!(
+       "Whatever.",
+       reply("This is a statement ending with whitespace      ")
+   );
+}
+
+// 8
+#[test]
+fn test_verse_0() {
+   assert_eq!(verse(0), "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+}
+
+#[test]
+//#[ignore]
+fn test_verse_1() {
+   assert_eq!(verse(1), "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n");
+}
+
+#[test]
+//#[ignore]
+fn test_verse_2() {
+   assert_eq!(verse(2), "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n");
+}
+
+#[test]
+//#[ignore]
+fn test_verse_8() {
+   assert_eq!(verse(8), "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n");
+}
+
+#[test]
+//#[ignore]
+fn test_song_8_6() {
+   assert_eq!(sing(8, 6), "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n");
+}
+
+#[test]
+//#[ignore]
+fn test_song_3_0() {
+   assert_eq!(sing(3, 0), "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+}
+
+// 9
+#[test]
+fn test_two_pieces() {
+   let input = vec!["nail", "shoe"];
+   let expected = vec![
+       "For want of a nail the shoe was lost.",
+       "And all for the want of a nail.",
+   ]
+   .join("\n");
+   assert_eq!(build_proverb(input), expected);
+}
+
+// Notice the change in the last line at three pieces.
+#[test]
+//#[ignore]
+fn test_three_pieces() {
+   let input = vec!["nail", "shoe", "horse"];
+   let expected = vec![
+       "For want of a nail the shoe was lost.",
+       "For want of a shoe the horse was lost.",
+       "And all for the want of a nail.",
+   ]
+   .join("\n");
+   assert_eq!(build_proverb(input), expected);
+}
+
+#[test]
+//#[ignore]
+fn test_one_piece() {
+   let input = vec!["nail"];
+   let expected = String::from("And all for the want of a nail.");
+   assert_eq!(build_proverb(input), expected);
+}
+
+#[test]
+//#[ignore]
+fn test_zero_pieces() {
+   let input: Vec<&str> = vec![];
+   let expected = String::new();
+   assert_eq!(build_proverb(input), expected);
+}
+
+#[test]
+//#[ignore]
+fn test_full() {
+   let input = vec![
+       "nail", "shoe", "horse", "rider", "message", "battle", "kingdom",
+   ];
+   let expected = vec![
+       "For want of a nail the shoe was lost.",
+       "For want of a shoe the horse was lost.",
+       "For want of a horse the rider was lost.",
+       "For want of a rider the message was lost.",
+       "For want of a message the battle was lost.",
+       "For want of a battle the kingdom was lost.",
+       "And all for the want of a nail.",
+   ]
+   .join("\n");
+   assert_eq!(build_proverb(input), expected);
+}
+
+#[test]
+//#[ignore]
+fn test_three_pieces_modernized() {
+   let input = vec!["pin", "gun", "soldier", "battle"];
+   let expected = vec![
+       "For want of a pin the gun was lost.",
+       "For want of a gun the soldier was lost.",
+       "For want of a soldier the battle was lost.",
+       "And all for the want of a pin.",
+   ]
+   .join("\n");
+   assert_eq!(build_proverb(input), expected);
+}
+
+// 10
+#[test]
+fn test_square_of_sum_1() {
+   assert_eq!(1, square_of_sum(1));
+}
+
+#[test]
+//#[ignore]
+fn test_square_of_sum_5() {
+   assert_eq!(225, square_of_sum(5));
+}
+
+#[test]
+//#[ignore]
+fn test_square_of_sum_100() {
+   assert_eq!(25502500, square_of_sum(100));
+}
+
+#[test]
+//#[ignore]
+fn test_sum_of_squares_1() {
+   assert_eq!(1, sum_of_squares(1));
+}
+
+#[test]
+//#[ignore]
+fn test_sum_of_squares_5() {
+   assert_eq!(55, sum_of_squares(5));
+}
+
+#[test]
+//#[ignore]
+fn test_sum_of_squares_100() {
+   assert_eq!(338350, sum_of_squares(100));
+}
+
+#[test]
+//#[ignore]
+fn test_difference_1() {
+   assert_eq!(0, difference(1));
+}
+
+#[test]
+//#[ignore]
+fn test_difference_5() {
+   assert_eq!(170, difference(5));
+}
+
+#[test]
+//#[ignore]
+fn test_difference_100() {
+   assert_eq!(25164150, difference(100));
+}
+
+// 11
+#[test]
+fn multiples_one() {
+   assert_eq!(0, sum_of_multiples(1, &[3, 5]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_two() {
+   assert_eq!(3, sum_of_multiples(4, &[3, 5]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_three() {
+   assert_eq!(23, sum_of_multiples(10, &[3, 5]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_four() {
+   assert_eq!(2318, sum_of_multiples(100, &[3, 5]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_five() {
+   assert_eq!(233168, sum_of_multiples(1000, &[3, 5]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_six() {
+   assert_eq!(51, sum_of_multiples(20, &[7, 13, 17]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_seven() {
+   assert_eq!(30, sum_of_multiples(15, &[4, 6]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_eight() {
+   assert_eq!(4419, sum_of_multiples(150, &[5, 6, 8]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_nine() {
+   assert_eq!(275, sum_of_multiples(51, &[5, 25]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_ten() {
+   assert_eq!(2203160, sum_of_multiples(10000, &[43, 47]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_eleven() {
+   assert_eq!(4950, sum_of_multiples(100, &[1]))
+}
+
+#[test]
+//#[ignore]
+fn multiples_twelve() {
+   assert_eq!(0, sum_of_multiples(10000, &[]))
+}
+
+// 12
+#[test]
+fn square_one() {
+   assert_eq!(square(1), 1);
+}
+
+#[test]
+//#[ignore]
+fn square_two() {
+   assert_eq!(square(2), 2);
+}
+
+#[test]
+//#[ignore]
+fn square_three() {
+   assert_eq!(square(3), 4);
+}
+
+#[test]
+//#[ignore]
+fn square_four() {
+   assert_eq!(square(4), 8);
+}
+
+#[test]
+//#[ignore]
+fn square_sixteen() {
+   assert_eq!(square(16), 32_768);
+}
+
+#[test]
+//#[ignore]
+fn square_thirty_two() {
+   assert_eq!(square(32), 2_147_483_648);
+}
+
+#[test]
+//#[ignore]
+fn square_sixty_four() {
+   assert_eq!(square(64), 9_223_372_036_854_775_808);
+}
+
+#[test]
+//#[ignore]
+#[should_panic(expected = "Square must be between 1 and 64")]
+fn square_zero_panics() {
+   square(0);
+}
+
+#[test]
+//#[ignore]
+#[should_panic(expected = "Square must be between 1 and 64")]
+fn square_sixty_five_panics() {
+   square(65);
+}
+
+#[test]
+//#[ignore]
+fn total_sums_all_squares() {
+   assert_eq!(total(), 18_446_744_073_709_551_615);
+}
