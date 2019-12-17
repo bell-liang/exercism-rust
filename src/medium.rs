ba2a6c347a13b20887fb62db0605f52373c7b54e
bell_liang <liang.foreverbell@qq.com>
2019-12-17 22:32:11 +0800

    medium 2

--- src/medium.rs
+++ src/medium.rs
@@ -1,3 +1,23 @@
+/* 1 鞍点
+    检测矩阵中的鞍点(saddle point).
+
+    所以说你有一个像这样的矩阵:
+
+        0  1  2
+    |---------
+    0 | 9  8  7
+    1 | 5  3  2     <--- saddle point at (1,0)
+    2 | 6  6  7
+    它在(1,0)处有一个鞍点.
+
+    它被称为”鞍点”,因为它是该 行 最大数,也是该 列 的最小数。
+
+    矩阵可以具有零个或多个鞍点.
+
+    您的代码应该能够为任何给定矩阵提供所有鞍点的(可能为空)列表。
+
+    矩阵可以具有不同数量的行和列(非正方形)
+*/
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let mut out: Vec<(usize, usize)> = vec![];
     let row = input.len();
@@ -12,4 +32,107 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
         }
     }
     out
+}
+/* 2 等值线
+    在等值线(又称为”无定形的 Word 是一个字或短语”)没有重复字母的单词或短语的逻辑术语，但允许连字符和空格出现多次
+*/
+pub fn check(candidate: &str) -> bool {
+    let mut result = true;
+    if candidate.len() == 0 {
+        return true;
+    }
+    let candidate = candidate.to_lowercase().chars().filter(|c| *c != '-' && *c != ' ').collect::<Vec<char>>();
+    for i in 0..(candidate.len()-1) {
+        if candidate.get((i+1)..).unwrap().contains(candidate.get(i).unwrap()){
+            result = false;
+            break;
+        }
+    }
+    result
+}
+/*
+pub fn check(word: &str) -> bool {
+   // Filter all non-Alphabetic character out and collect them in a new String
+   let normalized_string: String = word.to_lowercase()
+       .chars()
+       .filter(|c| c.is_alphabetic())
+       .collect();
+
+   /* Find the char element from back and front and compare the index.
+      If it is the same unique char the index will be the same.*/
+   let is_unique = |x: char, word: &str| word.find(x).unwrap() == word.rfind(x).unwrap();
+
+   // Length should be the same if it is a isogram
+   normalized_string.len()
+       == normalized_string
+           .chars()
+           .filter(|&x| is_unique(x, normalized_string.as_str()))
+           .count()
+}
+*/
+
+// 3 说
+// 提供一个 0 到 999,999,999,999 之间的数字,用英语拼出这个数字
+pub fn encode(n: u64) -> String {
+    let bit = |x| {
+        match x % 10 {
+            0 => "",
+            1 => "one",
+            2 => "two",
+            3 => "three",
+            4 => "four",
+            5 => "five",
+            6 => "six",
+            7 => "seven",
+            8 => "eight",
+            9 => "nine",
+        }
+    };
+    let out = match n / 10 {
+        0 => bit(n),
+        1 => match n % 10 {
+            0 => "ten",
+            1 => "eleven",
+            2 => "twelve",
+            3 => "thirteen",
+            4 => "fourteen",
+            5 => "fifteen",
+            6 => "sixteen",
+            7 => "seventeen",
+            8 => "eighteen",
+            9 => "nineteen",
+        },
+        2 => match n % 10 {
+            0 => "twenty",
+            _ => &format!("twenty-{}", bit(n))
+        },
+        3 => match n % 10 {
+            0 => "thirty",
+            _ => &format!("twenty-{}", bit(n))
+        },
+        4 => match n % 10 {
+            0 => "fourty",
+            _ => &format!("fourty-{}", bit(n))
+        },
+        5 => match n % 10 {
+            0 => "fifty",
+            _ => &format!("fifty-{}", bit(n))
+        },
+        6 => match n % 10 {
+            0 => "sixty",
+            _ => &format!("sixty-{}", bit(n))
+        },
+        7 => match n % 10 {
+            0 => "seventy",
+            _ => &format!("seventy-{}", bit(n))
+        },
+        8 => match n % 10 {
+            0 => "eighty",
+            _ => &format!("eighty-{}", bit(n))
+        },
+        9 => match n % 10 {
+            0 => "ninety",
+            _ => &format!("ninety-{}", bit(n))
+        },
+    };
 }
--- tests/medium_test.rs
+++ tests/medium_test.rs
@@ -3,86 +3,168 @@ use exercism_rust::medium::*;
 
 // 1
 fn find_sorted_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    let mut result = find_saddle_points(input);
-    println!("{:?}", result);
-    result.sort();
-    result
- }
- 
- #[test]
- fn identify_single_saddle_point() {
-    let input = vec![vec![9, 8, 7], vec![5, 3, 2], vec![6, 6, 7]];
-    assert_eq!(vec![(1, 0)], find_saddle_points(&input));
- }
- 
- #[test]
- //#[ignore]
- fn identify_empty_matrix() {
-    let input = vec![vec![], vec![], vec![]];
-    let expected: Vec<(usize, usize)> = Vec::new();
-    assert_eq!(expected, find_saddle_points(&input));
- }
- 
- #[test]
- //#[ignore]
- fn identify_lack_of_saddle_point() {
-    let input = vec![vec![1, 2, 3], vec![3, 1, 2], vec![2, 3, 1]];
-    let expected: Vec<(usize, usize)> = Vec::new();
-    assert_eq!(expected, find_saddle_points(&input));
- }
- 
- #[test]
- //#[ignore]
- fn multiple_saddle_points_in_col() {
-    let input = vec![vec![4, 5, 4], vec![3, 5, 5], vec![1, 5, 4]];
-    assert_eq!(
-        vec![(0, 1), (1, 1), (2, 1)],
-        find_sorted_saddle_points(&input)
-    );
- }
- 
- #[test]
- //#[ignore]
- fn multiple_saddle_points_in_row() {
-    let input = vec![vec![6, 7, 8], vec![5, 5, 5], vec![7, 5, 6]];
-    assert_eq!(
-        vec![(1, 0), (1, 1), (1, 2)],
-        find_sorted_saddle_points(&input)
-    );
- }
- 
- #[test]
- //#[ignore]
- fn identify_bottom_right_saddle_point() {
-    let input = vec![vec![8, 7, 9], vec![6, 7, 6], vec![3, 2, 5]];
-    assert_eq!(vec![(2, 2)], find_saddle_points(&input));
- }
- 
- // track specific as of v1.3
- #[test]
- //#[ignore]
- fn non_square_matrix_high() {
-    let input = vec![vec![1, 5], vec![3, 6], vec![2, 7], vec![3, 8]];
-    assert_eq!(vec![(0, 1)], find_saddle_points(&input));
- }
- 
- #[test]
- //#[ignore]
- fn non_square_matrix_wide() {
-    let input = vec![vec![3, 1, 3], vec![3, 2, 4]];
-    assert_eq!(vec![(0, 0), (0, 2)], find_sorted_saddle_points(&input));
- }
- 
- #[test]
- //#[ignore]
- fn single_column_matrix() {
-    let input = vec![vec![2], vec![1], vec![4], vec![1]];
-    assert_eq!(vec![(1, 0), (3, 0)], find_saddle_points(&input));
- }
- 
- #[test]
- //#[ignore]
- fn single_row_matrix() {
-    let input = vec![vec![2, 5, 3, 5]];
-    assert_eq!(vec![(0, 1), (0, 3)], find_saddle_points(&input));
- }
+   let mut result = find_saddle_points(input);
+   println!("{:?}", result);
+   result.sort();
+   result
+}
+
+#[test]
+fn identify_single_saddle_point() {
+   let input = vec![vec![9, 8, 7], vec![5, 3, 2], vec![6, 6, 7]];
+   assert_eq!(vec![(1, 0)], find_saddle_points(&input));
+}
+
+#[test]
+//#[ignore]
+fn identify_empty_matrix() {
+   let input = vec![vec![], vec![], vec![]];
+   let expected: Vec<(usize, usize)> = Vec::new();
+   assert_eq!(expected, find_saddle_points(&input));
+}
+
+#[test]
+//#[ignore]
+fn identify_lack_of_saddle_point() {
+   let input = vec![vec![1, 2, 3], vec![3, 1, 2], vec![2, 3, 1]];
+   let expected: Vec<(usize, usize)> = Vec::new();
+   assert_eq!(expected, find_saddle_points(&input));
+}
+
+#[test]
+//#[ignore]
+fn multiple_saddle_points_in_col() {
+   let input = vec![vec![4, 5, 4], vec![3, 5, 5], vec![1, 5, 4]];
+   assert_eq!(
+      vec![(0, 1), (1, 1), (2, 1)],
+      find_sorted_saddle_points(&input)
+   );
+}
+
+#[test]
+//#[ignore]
+fn multiple_saddle_points_in_row() {
+   let input = vec![vec![6, 7, 8], vec![5, 5, 5], vec![7, 5, 6]];
+   assert_eq!(
+      vec![(1, 0), (1, 1), (1, 2)],
+      find_sorted_saddle_points(&input)
+   );
+}
+
+#[test]
+//#[ignore]
+fn identify_bottom_right_saddle_point() {
+   let input = vec![vec![8, 7, 9], vec![6, 7, 6], vec![3, 2, 5]];
+   assert_eq!(vec![(2, 2)], find_saddle_points(&input));
+}
+
+// track specific as of v1.3
+#[test]
+//#[ignore]
+fn non_square_matrix_high() {
+   let input = vec![vec![1, 5], vec![3, 6], vec![2, 7], vec![3, 8]];
+   assert_eq!(vec![(0, 1)], find_saddle_points(&input));
+}
+
+#[test]
+//#[ignore]
+fn non_square_matrix_wide() {
+   let input = vec![vec![3, 1, 3], vec![3, 2, 4]];
+   assert_eq!(vec![(0, 0), (0, 2)], find_sorted_saddle_points(&input));
+}
+
+#[test]
+//#[ignore]
+fn single_column_matrix() {
+   let input = vec![vec![2], vec![1], vec![4], vec![1]];
+   assert_eq!(vec![(1, 0), (3, 0)], find_saddle_points(&input));
+}
+
+#[test]
+//#[ignore]
+fn single_row_matrix() {
+   let input = vec![vec![2, 5, 3, 5]];
+   assert_eq!(vec![(0, 1), (0, 3)], find_saddle_points(&input));
+}
+
+// 2
+#[test]
+fn empty_string() {
+   assert_eq!(check(""), true, "An empty string should be an isogram.")
+}
+
+#[test]
+//#[ignore]
+fn only_lower_case_characters() {
+   assert_eq!(check("isogram"), true, "\"isogram\" should be an isogram.")
+}
+
+#[test]
+//#[ignore]
+fn one_duplicated_character() {
+   assert_eq!(
+       check("eleven"),
+       false,
+       "\"eleven\" has more than one \'e\', therefore it is no isogram."
+   )
+}
+
+#[test]
+//#[ignore]
+fn longest_reported_english_isogram() {
+   assert_eq!(
+       check("subdermatoglyphic"),
+       true,
+       "\"subdermatoglyphic\" should be an isogram."
+   )
+}
+
+#[test]
+//#[ignore]
+fn one_duplicated_character_mixed_case() {
+   assert_eq!(
+       check("Alphabet"),
+       false,
+       "\"Alphabet\" has more than one \'a\', therefore it is no isogram."
+   )
+}
+
+#[test]
+//#[ignore]
+fn hypothetical_isogramic_word_with_hyphen() {
+   assert_eq!(
+       check("thumbscrew-japingly"),
+       true,
+       "\"thumbscrew-japingly\" should be an isogram."
+   )
+}
+
+#[test]
+//#[ignore]
+fn isogram_with_duplicated_hyphen() {
+   assert_eq!(
+       check("six-year-old"),
+       true,
+       "\"six-year-old\" should be an isogram."
+   )
+}
+
+#[test]
+//#[ignore]
+fn made_up_name_that_is_an_isogram() {
+   assert_eq!(
+       check("Emily Jung Schwartzkopf"),
+       true,
+       "\"Emily Jung Schwartzkopf\" should be an isogram."
+   )
+}
+
+#[test]
+//#[ignore]
+fn duplicated_character_in_the_middle() {
+   assert_eq!(
+       check("accentor"),
+       false,
+       "\"accentor\" has more than one \'c\', therefore it is no isogram."
+   )
+}
